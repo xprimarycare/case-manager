7cfb444d37247b26e0589faf8ea2bff5c07e0e90
rdubtrax <trachtmanr@yahoo.com>
2025-11-18 12:56:32 -0600

    add not found decorators for get by ID, patch, and delete endpoints

--- api/src/app/cases/cases.controller.ts
+++ api/src/app/cases/cases.controller.ts
@@ -11,6 +11,7 @@ import { CaseDto } from './dto/case.dto';
 import { CreateCaseDto } from './dto/create-case.dto';
 import { UpdateCaseDto } from './dto/update-case.dto';
 import { CasesService } from './cases.service';
+import { ApiNotFoundResponse } from '@nestjs/swagger';
 
 @Controller('cases')
 export class CasesController {
@@ -21,6 +22,7 @@ export class CasesController {
     return this.casesService.findAll();
   }
 
+  @ApiNotFoundResponse({ description: 'Case not found' })
   @Get(':id')
   findOne(@Param('id') id: string): CaseDto {
     return this.casesService.findOne(id);
@@ -31,11 +33,13 @@ export class CasesController {
     return this.casesService.create(createCaseDto);
   }
 
+  @ApiNotFoundResponse({ description: 'Case not found' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCaseDto: UpdateCaseDto) {
     return this.casesService.update(id, updateCaseDto);
   }
 
+  @ApiNotFoundResponse({ description: 'Case not found' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.casesService.remove(id);
