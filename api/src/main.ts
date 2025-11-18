22f54d01e6a1261bae13bf8dd75043baea304f74
rdubtrax <trachtmanr@yahoo.com>
2025-11-18 11:44:45 -0600

    add swagger scaffolding and remove .nx/cache from git

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,13 +2,23 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Case Management API')
+    .setDescription('Documentation for the case management API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
