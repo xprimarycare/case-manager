b79e808bc126a03f1813c9d368394ebca9ec7326
rdubtrax <trachtmanr@yahoo.com>
2025-12-05 15:37:32 -0600

    replace old implementation with Convex backend implementation

--- api/src/app/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CasesModule } from './cases/cases.module';
-import { PrismaModule } from './prisma/prisma.module';
-
-@Module({
-  imports: [CasesModule, PrismaModule],
-})
-export class AppModule {}
